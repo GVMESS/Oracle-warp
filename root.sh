8c6bd63e4151455e51bb20548a5bbca5e561234d
GVMESS <58140508+GVMESS@users.noreply.github.com>
2021-06-07 11:13:31 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -5,8 +5,8 @@ sudo chattr -i /etc/passwd /etc/shadow
 sudo chattr -a /etc/passwd /etc/shadow
 sudo lsattr /etc/passwd /etc/shadow
 
-read -p "自定义ROOT密码:" mima
-echo root:$mima | sudo chpasswd root
+#read -p "自定义ROOT密码:" mima
+echo root:"Google812!" | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo service sshd restart
